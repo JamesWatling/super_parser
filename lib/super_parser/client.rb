0ae01dc8ebbfa2d967643f6f5a7cf9907d648a36
James Watling <watling.james@gmail.com>
2023-06-21 11:49:25 -0600

    take a file rather than path

--- lib/super_parser/client.rb
+++ lib/super_parser/client.rb
@@ -5,25 +5,23 @@ require_relative 'client_error'
 
 module SuperParser # :nodoc:
   class Client
-    def parse(filepath)
+    def parse(file)
       uri = URI('https://api.superparser.com/parse')
       request = Net::HTTP::Post.new(uri)
       request['X-API-KEY'] = SuperParser.api_key
       request['accept'] = 'application/json'
 
-      File.open(filepath) do |file|
-        form_data = [['file_name', file]]
+      form_data = [['file_name', file]]
 
-        request.set_form form_data, 'multipart/form-data'
-        response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-          http.request(request)
-        end
+      request.set_form form_data, 'multipart/form-data'
+      response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+        http.request(request)
+      end
 
-        if response.code != 200
-          SuperParser::ClientError.new response
-        else
-          SuperParser::Response.new(response.body)
-        end
+      if response.code != 200
+        SuperParser::ClientError.new response
+      else
+        SuperParser::Response.new(response.body)
       end
     end
   end
